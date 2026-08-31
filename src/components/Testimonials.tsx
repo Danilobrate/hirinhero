998e553b0d25b189fce0b24501922dd8c337f3f6
Danilobrate <75435086+Danilobrate@users.noreply.github.com>
2026-08-31 23:38:00 +0200

    Testimonials: use carousel component, add t7-t9 to display order

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,25 +1,13 @@
 import {getTranslations, getLocale} from 'next-intl/server';
 import {getTestimonials} from '@/lib/sanity';
+import TestimonialsCarousel from './TestimonialsCarousel';
 
 export default async function Testimonials() {
   const t = await getTranslations();
   const locale = await getLocale();
   const items = await getTestimonials(locale);
-  const data = items ?? [4, 3, 2, 6, 5, 1].map(i => ({
+  const data = items ?? [4, 3, 2, 7, 9, 6, 5, 8, 1].map(i => ({
     quote: t(`t${i}_q`), name: t(`t${i}_n`), role: t(`t${i}_r`)
   }));
-  return (
-    <>
-      <div className="tcards">
-        {data.map((x, i) => (
-          <div className="tcard reveal" key={i}>
-            <div className="q">{x.quote}</div>
-            <div className="who">
-              <div><b>{x.name}</b><span>{x.role}</span></div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+  return <TestimonialsCarousel items={data} />;
 }
